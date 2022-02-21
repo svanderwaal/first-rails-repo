05e9a3e8f0406765f4875916f8723721605c3f6b
Sarah van der Waal <svanderwaal@hotmail.com>
2022-02-21 12:36:51 +1100

    created new rails app using ruby on rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  # @question = question
+
+  def ask
+    # puts params[:question].to_s
+    # puts '--You'
+  end
+
+  def answer
+    if params[:question].downcase == 'i am going to work'
+      @answer = 'Great!'
+    elsif params[:question]&.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
